eeee28b230376d0172618fc766adbea72325c547
Henrique Mateus Costa Araujo <20172TINF0309@ifpi.int>
2019-04-23 14:50:10 -0300

    adicionando icones e tab de topanimes

---
+++ src/app/pages/topanimes/topanimes.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TopanimesPage } from './topanimes.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TopanimesPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TopanimesPage]
+})
+export class TopanimesPageModule {}
---
+++ src/app/pages/topanimes/topanimes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topanimes',
+  templateUrl: './topanimes.page.html',
+  styleUrls: ['./topanimes.page.scss'],
+})
+export class TopanimesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
