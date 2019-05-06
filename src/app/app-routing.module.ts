1ac14dbed2b535358374a40945bfa2b0e4bcda5c
Elias Albert <sailetrebla@gmail.com>
2019-05-06 14:34:36 -0300

    add page de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'topanimes', loadChildren: './pages/topanimes/topanimes.module#TopanimesPageModule' },
-  { path: 'generos', loadChildren: './pages/generos/generos.module#GenerosPageModule' }
+  { path: 'generos', loadChildren: './pages/generos/generos.module#GenerosPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 
 ];
 @NgModule({
