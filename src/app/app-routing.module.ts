bca667c2a439155b80a50be92923f043e68d4f79
Elias Albert <sailetrebla@gmail.com>
2019-05-07 13:30:16 -0300

    add login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'topanimes', loadChildren: './pages/topanimes/topanimes.module#TopanimesPageModule' },
   { path: 'generos', loadChildren: './pages/generos/generos.module#GenerosPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
 
 ];
 @NgModule({
